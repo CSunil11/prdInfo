d3eb2833460a3fff8f50ef3b1b65519a1101672e
Sunil Chauhan <CSunil11@users.noreply.github.com>
2021-07-22 17:25:55 +0000

    master init

---
+++ src/main/java/com/prd/prdInfo/PrdInfoApplication.java
@@ -0,0 +1,13 @@
+package com.prd.prdInfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrdInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrdInfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prd/prdInfo/PrdInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prd.prdInfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrdInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
